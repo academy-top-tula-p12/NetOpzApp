9d94c1be669b8b8b7d4daae466fcde54ecadcbd4
maxefimov <efimov.top.academy@gmail.com>
2024-03-19 20:34:13 +0300

    19-03-24

--- NetOpzApp/MathExpression.cs
+++ NetOpzApp/MathExpression.cs
@@ -81,14 +81,176 @@ namespace NetOpzApp
         {
             Stack<char> operationsStack = new();
 
+            bool isUnar = true;
+            bool isLastOperator = false;
+
+
             for(int position = 0; position < Expression.Length; position++)
             {
-                if (Char.IsWhiteSpace(Expression[position])) continue;
+                char symbol = Expression[position];
 
-                double x = .5;
+                // white space chars
+                if (Char.IsWhiteSpace(symbol)) continue;
 
+                // unar minus
+                if (symbol == '-' && (isUnar || isLastOperator))
+                {
+                    ExpressionOpz += '~';
+                    isUnar = false;
+                    continue;
+                }
+                
+                // multiplex operators
+                if(operatorsMultiplex.Contains(symbol))
+                {
+                    while (operationsStack.Count > 0
+                            && operatorsMultiplex.Contains(operationsStack.Peek()))
+                        ExpressionOpz += operationsStack.Pop();
+                    operationsStack.Push(symbol);
+                    isLastOperator = true;
+                    isUnar = false;
+                    continue;
+                }
 
+                // additive operators
+                if(operatorsAdditive.Contains(symbol))
+                {
+                    while(operationsStack.Count > 0
+                          && (operatorsMultiplex.Contains(operationsStack.Peek())
+                          || operatorsAdditive.Contains(operationsStack.Peek())))
+                        ExpressionOpz += operationsStack.Pop();
+                    operationsStack.Push(symbol);
+                    isLastOperator = true;
+                    isUnar = false;
+                    continue;
+                }
+
+                // opening brackets
+                if (bracketsOpen.Contains(symbol))
+                {
+                    operationsStack.Push(symbol);
+                    isUnar = true;
+                    isLastOperator = false;
+                    continue;
+                }
+                
+                // closing brackets
+                if(bracketsClose.Contains(symbol))
+                {
+                    while (!bracketsOpen.Contains(operationsStack.Peek()))
+                        ExpressionOpz += operationsStack.Pop();
+                    operationsStack.Pop();
+                    isUnar = false;
+                    isLastOperator = false;
+                    continue;
+                }
+
+                // numbers
+                if(Char.IsDigit(symbol) || symbol == '.') 
+                {
+                    string number = "";
+                    while((Char.IsDigit(symbol)
+                          || symbol == '.'
+                          || Char.ToLower(symbol) == 'e')
+                          && position < Expression.Length)
+                    {
+                        
+                        if (Char.ToLower(symbol) == 'e'
+                            && Expression[position + 1] == '-')
+                        {
+                            number += "e-";
+                            position += 2;
+                            symbol = Expression[position];
+                        }
+                        else
+                        {
+                            if (symbol == '.')
+                                number += ',';
+                            else
+                                number += symbol;
+                            ++position;
+                            if (position < Expression.Length)
+                                symbol = Expression[position];
+                        }
+                    }
+                    number += '#';
+                    ExpressionOpz += number;
+                    position--;
+                    isUnar = false;
+                    isLastOperator = false;
+                    continue;
+                }
+
+                // variables
+                if(Char.IsLetter(symbol)
+                    || symbol == '_')
+                {
+                    string name = "";
+                    while((Char.IsLetterOrDigit(symbol)
+                          || symbol == '_')
+                          && position < Expression.Length)
+                    {
+                        name += symbol;
+                        ++position;
+                        if (position < Expression.Length)
+                            symbol = Expression[position];
+                    }
+                    ExpressionOpz += variables[name].ToString();
+                    ExpressionOpz += "#";
+                    position--;
+                    isUnar = false;
+                    isLastOperator = false;
+                }
             }
+
+            while (operationsStack.Count > 0)
+                ExpressionOpz += operationsStack.Pop();
+        }
+
+        public double OpzCalculate()
+        {
+            Stack<double> operandsStack = new Stack<double>();
+
+            char symbol;
+            for (int position = 0; position < ExpressionOpz.Length; position++)
+            {
+                symbol = ExpressionOpz[position];
+                
+                // numbers
+                if (Char.IsDigit(symbol)
+                    || symbol == '~'
+                    || symbol == '.')
+                {
+                    string value = "";
+                    while(symbol != '#')
+                    {
+                        value += symbol;
+                        symbol = ExpressionOpz[++position];
+                    }
+                    operandsStack.Push(Double.Parse(value));
+                    //position++;
+                    continue;
+                }
+
+                // operators
+                if(operatorsAdditive.Contains(symbol)
+                    || operatorsMultiplex.Contains(symbol))
+                {
+                    double operand2 = operandsStack.Pop();
+                    double operand1 = operandsStack.Pop();
+
+                    double result = symbol switch
+                    {
+                        '-' => operand1 - operand2,
+                        '+' => operand1 + operand2,
+                        '*' => operand1 * operand2,
+                        '/' => operand1 / operand2
+                    };
+
+                    operandsStack.Push(result);
+                }
+            }
+            return operandsStack.Pop();
         }
     }
 }
--- NetOpzApp/Program.cs
+++ NetOpzApp/Program.cs
@@ -2,3 +2,12 @@
 
 MathExpression mathExpression = new MathExpression();
 
+mathExpression["a"] = 10.9;
+mathExpression["b"] = 4.75;
+mathExpression["c"] = 3;
+mathExpression.Expression = "a + b";
+mathExpression.OpzCreate();
+
+Console.WriteLine(mathExpression.ExpressionOpz);
+Console.WriteLine(mathExpression.OpzCalculate());
+
